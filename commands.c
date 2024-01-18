f6388ce25506ad6ffc51f0fe891a292b78ee9d2f
thalia_smt <113176525+tsimitop@users.noreply.github.com>
2024-01-18 20:50:17 +0100

    Add files via upload

---
+++ commands.c
@@ -0,0 +1,7 @@
+#include "push_swap.h"
+
+/*
+sa (swap a): Swap the first 2 elements at the top of stack a.
+Do nothing if there is only one or no elements.
+*/
+
---
+++ push_swap.h
@@ -0,0 +1,17 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "libft.h"
+
+/*commands*/
+
+
+/*struct nodes*/
+typedef struct	stack_node
+{
+	int					*integer;
+	struct stack_node	*next;
+	struct stack_node	*prev;
+}				stack_node
+
+#endif
